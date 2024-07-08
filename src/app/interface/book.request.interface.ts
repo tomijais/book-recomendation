9cb87d27a648e37a96057c1bb7aaa59f442ade17
Tomas Jais <tomijais@gmail.com>
2024-07-08 13:33:45 -0300

    feat: create BookRecomendation interface

---
+++ src/app/interface/book.request.interface.ts
@@ -0,0 +1,9 @@
+export interface BookRequest {
+    titles: string[];
+}
+export interface BookRecommendation {
+    book: string;
+    author: string;
+    rating: string;
+    description: string;
+  }
